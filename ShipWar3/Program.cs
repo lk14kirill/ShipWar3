7a57fc238d1d4e046543eb652f0b7a83500d6d36
lakerbtw <addky11@gmail.com>
2021-04-02 22:04:01 +0300

    Vers 1.2 Added player types Game till 3

--- ShipWar3/Program.cs
+++ ShipWar3/Program.cs
@@ -6,8 +6,8 @@ namespace ShipWar3
     {
         static void Main(string[] args)
         {
-            var gameCycle = new GameCycle();
-            gameCycle.StartGame();
+            var lobby = new Lobby();
+            lobby.Start();
         }
     }
     public enum ShipState
@@ -16,7 +16,32 @@ namespace ShipWar3
         destroyed,
         unspawned
     }
-    public struct Consts
+    public enum PlayerType
+    {
+        human,
+        ai
+    }
+    struct Player
+    {
+        public PlayerType typeOfPlayer;
+        public Ship[]? ships;
+        public FrontendField? field;
+        public string name;
+        public int wins;
+        public Player(PlayerType typeOfPlayer, Ship[] ships, FrontendField field, string name,  int wins)
+        {
+            this.typeOfPlayer = typeOfPlayer;
+            this.ships = ships;
+            this.field = field;
+            this.name = name;
+            this.wins = wins;
+            if (typeOfPlayer == PlayerType.ai)
+            {
+                this.name = "Bot";
+            }
+        }
+    }
+    public struct Constants
     {
         public const int width = 11;
         public const int height = 11;
@@ -27,7 +52,7 @@ namespace ShipWar3
         public int yCoord;
         public int size;
         public ShipState state;
-        
+
         public Ship(int size, int yCoord, int xCoord, ShipState state)
         {
             this.size = size;
@@ -57,7 +82,7 @@ namespace ShipWar3
         {
             for (int i = 0; i < number; i++)
             {
-                if(ships[number].xCoord == xCoord || ships[number].yCoord == yCoord)
+                if (ships[number].xCoord == xCoord || ships[number].yCoord == yCoord)
                 {
                     return false;
                 }
@@ -65,7 +90,7 @@ namespace ShipWar3
             return true;
         }
         public static int GenerateCoordinates()
-        { 
+        {
             Random rand = new Random();
             int coord = rand.Next(1, 10);
 
@@ -74,12 +99,12 @@ namespace ShipWar3
     }
     public class FrontendField
     {
-        public char[,] field = new char[Consts.height, Consts.width];
+        public char[,] field = new char[Constants.height, Constants.width];
         public void GenerateField()
         {
-            for (int y = 0; y < Consts.height; y++)
+            for (int y = 0; y < Constants.height; y++)
             {
-                for (int x = 0; x < Consts.width; x++)
+                for (int x = 0; x < Constants.width; x++)
                 {
                     field[y, x] = '.';
                 }
@@ -89,10 +114,7 @@ namespace ShipWar3
     public class Draw            //Method is doing drawing of this game
     {
         public char symbol;
-        public void MakeIndent()
-        {
-            Console.WriteLine();
-        }
+
         private int[][] TransformShipCoordInInts(Ship[] shipsArray)
         {
             int[][] intArray = new int[shipsArray.Length][];
@@ -104,7 +126,7 @@ namespace ShipWar3
             }
             return intArray;
         }
-        private int[] TransformShipStatesInArray (Ship[] shipsArray)
+        private int[] TransformShipStatesInArray(Ship[] shipsArray)
         {
             int[] shipsArrayInt = new int[shipsArray.Length];
             for (int i = 0; i < shipsArrayInt.Length; i++)
@@ -124,14 +146,18 @@ namespace ShipWar3
             }
             return shipsArrayInt;
         }
-        public void DrawFieldWithoutShips(ref FrontendField field)
+        public void DrawField(Ship[] ships, FrontendField field, bool areShipsHidden)
         {
+
+            int[] shipStates = TransformShipStatesInArray(ships);
+            int[][] shipCoords = TransformShipCoordInInts(ships);
             int numberToChar = 63;
             int number = 47;
-            for (int y = 0; y < Consts.height - 1; y++)
+            int f = 0;
+            for (int y = 0; y < Constants.height - 1; y++)
             {
                 number++;
-                for (int x = 0; x < Consts.width; x++)
+                for (int x = 0; x < Constants.width; x++)
                 {
                     numberToChar++;
 
@@ -144,51 +170,19 @@ namespace ShipWar3
                     {
                         symbol = (char)number;
                     }
-                    if (symbol == '.' || symbol == '^')
-                    {
-                        field.field[y, x] = symbol;
-                    }
-                    Console.Write(symbol);
-                }
-            Console.WriteLine();
-            }
-        }
-        public void DrawFieldWithShips(Ship[] ships,ref FrontendField field)
-        {
-            
-            int[] shipStates = TransformShipStatesInArray(ships);
-            int[][] shipCoords = TransformShipCoordInInts(ships);
-            int numberToChar = 63;
-            int number = 47;
-            int f = 0;
-            for (int y = 0; y < Consts.height - 1; y++) 
-            {
-                number++;
-                for (int x = 0; x < Consts.width; x++)
-                {
-                    numberToChar++;
-
-                    symbol = field.field[y,x];
-                    if (y == 0 && x >= 1)
-                    {
-                        symbol = (char)numberToChar;
-                    }
-                    if (y >= 1 && x == 0)
-                    {
-                        symbol = (char)number;
-                    }
-                    for (int i = 0; i < ships.Length; i++)
-                    {
-                        if (shipCoords[i][0] == x && shipCoords[i][1] == y)
+                    if (!areShipsHidden)
+                        for (int i = 0; i < ships.Length; i++)
                         {
-                            if(shipStates[i] == 0)
-                                symbol = '!';
-                            if (shipStates[i] == 1)
-                                symbol = '#';
-                            if (shipStates[i] == 2)
-                                symbol = '.';
+                            if (shipCoords[i][0] == x && shipCoords[i][1] == y)
+                            {
+                                if (shipStates[i] == 0)
+                                    symbol = '!';
+                                if (shipStates[i] == 1)
+                                    symbol = '#';
+                                if (shipStates[i] == 2)
+                                    symbol = '.';
+                            }
                         }
-                    }
                     if (symbol == '.' || symbol == '^')
                     {
                         field.field[y, x] = symbol;
@@ -208,7 +202,6 @@ namespace ShipWar3
             WriteASentence(ConsoleColor.Cyan, "You can play agains our bot.Here some instructions:");
             WriteASentence(ConsoleColor.Cyan, "^ - means you missed");
             WriteASentence(ConsoleColor.Cyan, "# - means you hit");
-            WriteASentence(ConsoleColor.Cyan, "There are no stable quantity of ships,it can either 3,nor 5.But not more than 11");
             WriteASentence(ConsoleColor.Cyan, "Every ship is single-deck");
             WriteASentence(ConsoleColor.Cyan, "First,who destroyed all of ships - wins!");
             Console.WriteLine();
@@ -220,63 +213,146 @@ namespace ShipWar3
             Console.WriteLine(text);
             Console.ResetColor();
         }
-        public (int,int)AskForCoordToShoot()
+        public (int, int) AskForCoordToShoot()
         {
-            int attackHeight = int.Parse(Console.ReadLine());
-            char letter = char.Parse(Console.ReadLine());
+            int attackHeight;
+            char letter;
+            while(!int.TryParse(Console.ReadLine(),out  attackHeight) || attackHeight > 10)
+                WriteASentence(ConsoleColor.Cyan, "Write a number within the field!");
+            while(!char.TryParse(Console.ReadLine(),out  letter) || (int) letter -64 > 10)
+                WriteASentence(ConsoleColor.Cyan, "Write a letter within the field!");
             int attackWidth = (int)letter - 64;
 
             return (attackWidth, attackHeight);
         }
         public void End()
         {
-            WriteASentence(ConsoleColor.Cyan, "Thanks for playing");    
+            WriteASentence(ConsoleColor.Cyan, "Thanks for playing");
+        }
+        public void TakeInfoAboutPlayers(out PlayerType player1Type, out PlayerType player2Type, out string? player1Name, out string? player2Name)
+        {
+            string player1Input;
+            string player2Input;
+            WriteASentence(ConsoleColor.Cyan, "Now choose who ll play:human or bot?");
+
+            do
+            {
+                WriteASentence(ConsoleColor.Cyan, "Please,enter 'human' or 'bot'");
+                player1Input = Console.ReadLine();
+            }
+            while (player1Input != "human" && player1Input != "bot");
+            ConvertStringAndDefineType(out player1Type, player1Input, out player1Name);
+            WriteASentence(ConsoleColor.Cyan, "Enter second player");
+            do
+            {
+                WriteASentence(ConsoleColor.Cyan, "Please,enter 'human' or 'bot'");
+                player2Input = Console.ReadLine();
+            }
+            while (player2Input != "human" && player2Input != "bot");
+            ConvertStringAndDefineType(out player2Type, player2Input, out player2Name);
+        }
+        public void ConvertStringAndDefineType(out PlayerType playerType, string input, out string? name)
+        {
+            playerType = PlayerType.ai;
+            name = null;
+            switch (input)
+            {
+                case "human":
+                    playerType = PlayerType.human;
+                    WriteASentence(ConsoleColor.Cyan, "Write your name please");
+                    name = Console.ReadLine();
+
+                    break;
+                case "bot":
+                    playerType = PlayerType.ai;
+                    name = null;
+                    break;
+            }
         }
     }
-    class GameCycle
+    class Lobby
+    {
+        private UI ui = new UI();
+        private GameSettings gameSettings = new GameSettings();
+        private PlayerType _player1Type;
+        private PlayerType _player2Type;
+        private string? _player1Name;
+        private string? _player2Name;
+        public void Start()
+        {
+            gameSettings.Menu();
+            Console.Clear();
+            BeginSettings();
+        }
+        public void BeginSettings()
+        {
+            ui.TakeInfoAboutPlayers(out _player1Type, out _player2Type, out _player1Name, out _player2Name);   // Takes info about player
+            gameSettings.DefinePlayers(_player1Type, _player2Type, _player1Name, _player2Name);                // Setting parameters for players
+
+            gameSettings.PlayGames(3);
+        }
+    }
+    class Game
     {
         private Ship shipReference = new Ship();
         private UI uiReference = new UI();
         private Draw drawClassReference = new Draw();
 
-        public static bool isgameEnded = false;
-        public int shipQuantity;
+        public static bool isgameEnded;
+        private int _shipQuantity;
         private int counter = 0;
+        private int cachedWinsPlayer1;
+        private int cachedWinsPlayer2;
 
-        public Ship[] playerShips;
-        public Ship[] AIShips;
-
-        public FrontendField playerField;
-        public FrontendField AIField;
-        private void Menu()
+        private Player _player1;
+        private Player _player2;
+        public Game(Player player1,Player player2,int shipQuantity)
         {
-            uiReference.GameSettings();
-            int start = int.Parse(Console.ReadLine());
-            shipQuantity = start;
+            _player1 = player1;
+            _player2 = player2;
+            _shipQuantity = shipQuantity;
         }
-        public  void StartGame()
+        public void CalculateWinner(ref Player player1,ref Player player2)
         {
-            Menu();
+            if (_player1.wins > cachedWinsPlayer1)
+                player1.wins = _player1.wins;
+            if (_player2.wins > cachedWinsPlayer2)
+                player2.wins = _player2.wins;
+        }
+        public void Start()
+        {
+            isgameEnded = false;
+            cachedWinsPlayer1 = _player1.wins;
+            cachedWinsPlayer2 = _player2.wins;
             Console.Clear();
+            uiReference.WriteASentence(ConsoleColor.Cyan, "Warming ships...");
+            System.Threading.Thread.Sleep(3000);
 
-            playerShips = shipReference.GenerateShipsWithCoordinates(shipQuantity);
-            playerField = new FrontendField();
-            playerField.GenerateField();
+            _player1.ships = shipReference.GenerateShipsWithCoordinates(_shipQuantity);
+            _player1.field = new FrontendField();
+            _player1.field.GenerateField();
 
-            AIShips = shipReference.GenerateShipsWithCoordinates(shipQuantity);
-            AIField = new FrontendField();
-            AIField.GenerateField();
+            _player2.ships = shipReference.GenerateShipsWithCoordinates(_shipQuantity);
+            _player2.field = new FrontendField();
+            _player2.field.GenerateField();
 
-            DrawingFieldLocalFunction();
+            DrawFields();
 
             GameProcess();
         }
-        private void DrawingFieldLocalFunction()                                                   //this Method calls drawing methods from drawclass
+        private void DrawFields()
         {
             Console.Clear();
-            drawClassReference.DrawFieldWithShips(playerShips, ref playerField);
-            drawClassReference.MakeIndent();
-            drawClassReference.DrawFieldWithoutShips(ref AIField);
+            DrawMethodWithDefiningToShowShipsOrNot(_player1);
+            Console.WriteLine();
+            DrawMethodWithDefiningToShowShipsOrNot(_player2);
+        }
+        private void DrawMethodWithDefiningToShowShipsOrNot(Player player)                                                   //Method which defines player type and drawing field depending on type
+        {
+            if (player.typeOfPlayer == PlayerType.ai)
+                drawClassReference.DrawField(player.ships, player.field, true);
+            else
+                drawClassReference.DrawField(player.ships, player.field, false);
         }
         public void GameProcess()
         {
@@ -286,38 +362,43 @@ namespace ShipWar3
             }
             uiReference.End();
         }
-
+        private (int x, int y) Attack(PlayerType playertype)
+        {
+            int x;
+            int y;
+            if (playertype == PlayerType.human)
+                (x, y) = uiReference.AskForCoordToShoot();
+            else
+                (x, y) = GenerateAttackCoords();
+            return (x, y);
+        }
         private void Cycle()
         {
             if (counter == 0)
             {
+                uiReference.WriteASentence(ConsoleColor.Cyan, _player1.name + " s turn!");
                 System.Threading.Thread.Sleep(1500);
-                (int x,int y) = uiReference.AskForCoordToShoot();
-                CheckForHit(ref AIShips,ref AIField, x, y);
-                DrawingFieldLocalFunction();
-                CheckForWinner(AIShips,"You ");
+                (int x, int y) = Attack(_player1.typeOfPlayer);
+                CheckForHit(_player2.ships, _player2.field, x, y);
+                DrawFields();
+                CheckForWinner(_player2.ships, ref _player1);
                 counter++;
             }
             else
             {
-                WaitForBot();
-                (int x, int y) = GenerateAttackCoords();
-                CheckForHit(ref playerShips, ref playerField, x, y);
-                DrawingFieldLocalFunction();
-                CheckForWinner(playerShips,"Enemy ");
+                uiReference.WriteASentence(ConsoleColor.Cyan, _player2.name + " s turn!");
+                (int x, int y) = Attack(_player2.typeOfPlayer);
+                CheckForHit(_player1.ships, _player1.field, x, y);
+                DrawFields();
+                CheckForWinner(_player1.ships, ref _player2);
                 counter--;
             }
         }
-        private void WaitForBot()
-        {
-            uiReference.WriteASentence(ConsoleColor.Cyan, "Wait,enemy is attacking you!");
-            System.Threading.Thread.Sleep(1500);
-        }
-        private void CheckForHit(ref Ship[] ships,ref FrontendField field,int x,int y)
+        private void CheckForHit(Ship[] ships, FrontendField field, int x, int y)
         {
             for (int i = 0; i < ships.Length; i++)
             {
-                if(ships[i].xCoord == x && ships[i].yCoord == y)
+                if (ships[i].xCoord == x && ships[i].yCoord == y)
                 {
                     ships[i].state = ShipState.destroyed;
                     field.field[y, x] = '!';
@@ -336,26 +417,59 @@ namespace ShipWar3
             uiReference.WriteASentence(ConsoleColor.Cyan, "Miss!");
             field.field[y, x] = '^';
         }
-        private (int x,int y) GenerateAttackCoords()
+        private (int x, int y) GenerateAttackCoords()
         {
             Random rand = new Random();
-            int x = rand.Next(1, Consts.width);
-            int y = rand.Next(1, Consts.height);
+            int x = rand.Next(1, Constants.width);
+            int y = rand.Next(1, Constants.height);
             return (x, y);
         }
-        public  bool isGameEnded(ref bool _isGameEnded)
+        public bool isGameEnded(ref bool _isGameEnded)
         {
             return _isGameEnded;
         }
-        private void CheckForWinner(Ship[] ships,string winner)
+        private void CheckForWinner(Ship[] ships, ref Player player)
         {
             for (int i = 0; i < ships.Length; i++)
             {
                 if (ships[i].state == ShipState.alive)
                     return;
             }
-            uiReference.WriteASentence(ConsoleColor.Cyan, winner + "became winner");
+            uiReference.WriteASentence(ConsoleColor.Cyan, player.name + " became winner");
+            player.wins += 1;
+            System.Threading.Thread.Sleep(1500);
             isgameEnded = true;
         }
+    }
+    class GameSettings
+    {
+        Player player1;
+        Player player2;
+        int shipQuantity;
+        private UI uiReference = new UI();
+        public void DefinePlayers(PlayerType player1Type, PlayerType player2Type, string player1Name, string player2Name)
+        {
+            player1 = new Player(player1Type, null, null, player1Name, 0); 
+            player2 = new Player(player2Type, null, null, player2Name, 0);
+        }
+        public void Menu()
+        {
+            uiReference.GameSettings();
+            while (!int.TryParse(Console.ReadLine(),out shipQuantity))
+               uiReference.WriteASentence(ConsoleColor.Cyan, "Write a number!");
+        }
+        public void PlayGames(int quantity)
+        {
+            while (player1.wins != quantity && player2.wins != quantity) 
+            {
+                Game game = new Game(player1, player2, shipQuantity);                                                  //creates new game and launches it
+                game.Start();
+                game.CalculateWinner(ref player1, ref player2);
+                uiReference.WriteASentence(ConsoleColor.Cyan, player1.name + ":" + player1.wins.ToString());           //showing results of game
+                uiReference.WriteASentence(ConsoleColor.Cyan, player2.name + ":" + player2.wins.ToString());
+                System.Threading.Thread.Sleep(1500);
+            }     
+        }
+
     }
 }
